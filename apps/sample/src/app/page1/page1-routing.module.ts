78d6de6677fb82af5eca5628c2eab8ef70a23be8
Vamsi Devalla <vamsi.devalla@cohesity.com>
2022-09-13 21:18:40 -0400

    nav bar, routing and adding material

--- apps/sample/src/app/page1/page1-routing.module.ts
+++ apps/sample/src/app/page1/page1-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Page1Component } from './page1.component';
+import { SecondaryComponent } from './secondary/secondary.component';
 
 const routes: Routes = [
   {
     path: '',
     component: Page1Component,
+  },
+  {
+    outlet: 'top-bar',
+    path: '',
+    component: SecondaryComponent,
   },
 ];
 
--- apps/sample/src/app/page2/page2-routing.module.ts
+++ apps/sample/src/app/page2/page2-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Page2Component } from './page2.component';
+import { SecondaryComponent } from './secondary/secondary.component';
 
 const routes: Routes = [
   {
     path: '',
     component: Page2Component,
+  },
+  {
+    outlet: 'side-bar',
+    path: '',
+    component: SecondaryComponent,
   },
 ];
 
